4e0763cc0f79ef3e16d5fef8f30e367e453218f6
zhaoxin0311 <1204090769@qq.com>
2021-10-03 15:35:05 +0800

    字符串模板

--- src/index.js
+++ src/index.js
@@ -22,11 +22,24 @@ let b=2
 let res=`${a+b} <br>`
 document.write(res)
 
+console.log('2. 字符串的查找（支持中文查找）');
+// es5 写法
+let jspang4='技术胖';
+let blog4 = '非常高兴你能看到这篇文章，我是你的老朋友技术胖。这节课我们学习字符串模版。';
+document.write(blog4.indexOf(jspang4));
+// 返回的是 索引的位置 20 此方法不实用
 
+// es6写法
+let jspang5='技术胖';
+let blog5 = '非常高兴你能看到这篇文章，我是你的老朋友技术胖';
+document.write(blog5.includes(jspang5));
 
+// *判断开头是否存在： *
+document.write(blog5.startsWith(jspang5));
+// 判断结尾是否存在：
+document.write(blog5.endsWith(jspang5))
+// 需要注意的是：starts和ends 后边都要加s，
 
-
-
-
-
+// ** 复制字符串**
+document.write('jspang5|'.repeat(3))
 
